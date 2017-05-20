c853f77360a20bfc7a3f74ce54221e1ce5f457b7
wabarr <wabarr@gmail.com>
2017-05-19 20:49:51 -0400

    switch over from person connection structure to single self-refferential PhD table

--- academicPhylogeny/admin.py
+++ academicPhylogeny/admin.py
@@ -2,9 +2,18 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from academicPhylogeny.models import *
+from ajax_select import make_ajax_form
 
 # Register your models here.
 
+class PhDAdmin(admin.ModelAdmin):
+    search_fields = ["lastName","firstName"]
+    list_filter = ["school"]
+    list_display = ["id","firstName", "lastName","year", "school", "validated"]
+    form = make_ajax_form(PhD, {
+        'advisor': 'person',  # ManyToManyField
+        'school': 'school'  # ForeignKeyField
+    })
 class SchoolAdmin(admin.ModelAdmin):
     search_fields = ["Name",]
 
@@ -17,4 +26,4 @@ class FAQadmin(admin.ModelAdmin):
 admin.site.register(frequently_asked_question,FAQadmin)
 admin.site.register(school, SchoolAdmin)
 admin.site.register(person, admin.ModelAdmin)
-admin.site.register(PhD, admin.ModelAdmin)
+admin.site.register(PhD, PhDAdmin)
