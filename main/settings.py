d7a0d740a7f354b9a59960886357c717b4ae31a0
wabarr <wabarr@gmail.com>
2018-10-16 19:39:35 -0400

    change graph api

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = secrets.DEBUG
 
-ALLOWED_HOSTS = ['127.0.0.1','www.physanthphylogeny.org']
+ALLOWED_HOSTS = ['127.0.0.1','www.physanthphylogeny.org', 'www.bioanthtree.org','bioanthtree.org']
 
 
 # Application definition
